4aafceda55adb0cf766e14ef5a09440528233d07
khalil <khalil94bouzayeni^gmail.com>
2021-11-16 11:20:23 +0100

    state

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { Component } from 'react'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends Component {
+state={
+fullName:'Khalil',
+bio:'from Regueb',
+profession:'web developper',
+show:false,
+count:0,
+}
+ showProfil=()=>{
+  this.setState({ show: !(this.state.show) })
+}
+componentDidMount(){
+  setInterval(() => {
+    this.setState({count: this.state.count +1});
+  }, 1000);
 }
 
-export default App;
+  render() {
+    return (
+      <div>
+       {this.state.show ? <div><p>{this.state.fullName}</p>
+        <p>{this.state.bio}</p>
+        <p>{this.state.profession}</p>
+        <img src= 'https://f.hellowork.com/blogdumoderateur/2019/03/code-ia-664x441.jpg'/></div>: null}
+        <button onClick={()=>this.showProfil()}>show</button>
+        <p>
+       Time interval since the component was mounted: {this.state.count}
+       </p> 
+      </div>
+    )
+  }
+}
+
